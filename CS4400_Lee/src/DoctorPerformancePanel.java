f4678b08552842c012af39e4a67e9ff54b6a6e97
rbordianu3@gmail.com <rbordianu3@gmail.com@b5539f44-ac4a-a273-9826-d54d70205aab>
2014-04-23 23:33:30 +0000

    fixed DoctorPerformancePanel queries

--- CS4400_Lee/src/DoctorPerformancePanel.java
+++ CS4400_Lee/src/DoctorPerformancePanel.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Vector;
 
 import javax.swing.JPanel;
@@ -13,31 +14,43 @@ import javax.swing.table.DefaultTableModel;
 public class DoctorPerformancePanel extends JPanel {
 	private JPanel panel;
 	private Connection con;
+	private ArrayList<String> specialties;
 	
 	public DoctorPerformancePanel(JPanel panel, Connection con){
 		this.panel = panel;
 		this.con = con;
 		panel.setLayout(new FlowLayout());
+		specialties = new ArrayList<String>();
 		
 		DefaultTableModel model = new DefaultTableModel();
 		model.addColumn("Specialty");
 		model.addColumn("Average Rating");
 		model.addColumn("# of Surgeries");
 		
-		String query = "Select Specialty, rate, Count(p.D_LicenseNumber) FROM (SELECT Specialty, AVG(Rating) as rate,LicenseNumber FROM Doctor as d, Rates as r WHERE LicenseNumber = r.D_licenseNumber GROUP BY Specialty) as dr INNER JOIN Performs as p on dr.LicenseNumber = p.D_LicenseNumber GROUP BY Specialty"; 
+		String rateQuery = "SELECT Specialty, AVG(Rating) FROM Doctor as d LEFT JOIN Rates as r ON LicenseNumber = r.D_licenseNumber GROUP BY Specialty"; 
+		String surgeryQuery = "SELECT Specialty, COUNT(d_licensenumber) FROM Doctor,Performs WHERE LicenseNumber = D_licenseNumber GROUP BY Specialty";
 		Statement stmt;
 		try {
 			stmt = con.createStatement();
-			ResultSet rates = stmt.executeQuery(query);
+			ResultSet rates = stmt.executeQuery(rateQuery);
 			
 			while(rates.next()){
 				Vector<String> vector = new Vector<String>();
 				vector.add(rates.getString(1));
 				vector.add(rates.getString(2));
-				vector.add(rates.getString(3));
+				vector.add("0");
+				specialties.add(rates.getString(1));
 				
 				model.addRow(vector);
 			}
+			
+			ResultSet surgeries = stmt.executeQuery(surgeryQuery);
+			while(surgeries.next()){
+				int i = specialties.indexOf(surgeries.getString(1));
+				String test = surgeries.getString(1);
+				model.setValueAt(surgeries.getString(2), i, 2);
+			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
